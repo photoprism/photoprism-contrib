bc489c4fea58bcd3cce8feb2a62400de80a1da91
Alessandro <alessandro@gnagni.it>
2025-08-07 11:49:15 +0200

    Update heic2jpg.sh
    Made to work recursive on big libraries

--- scripts/Batch Rename/heic2jpg.sh
+++ scripts/Batch Rename/heic2jpg.sh
@@ -3,6 +3,6 @@
 # This script changes the file extension of all JPEG images that incorrectly
 # match the "IMG_*.HEIC" file name pattern from ".HEIC" to ".JPG".
 
-for file in $(file IMG_*.HEIC | grep JPEG | cut -d ':' -f1); do
+for file in $(find . -type f -name "*.heic" -print0 | xargs -0 file | grep JPEG | cut -d ':' -f1); do
   mv "$file" "${file%.*}.JPG" ;
 done;
