5d63929a08861e2ddc2395fd882d19fbfe40e0f6
Xie Yanbo <xieyanbo@gmail.com>
2025-08-17 15:00:13 +0800

    feat: update heic2jpg.sh to support all subdirectories

--- scripts/Batch Rename/heic2jpg.sh
+++ scripts/Batch Rename/heic2jpg.sh
@@ -2,7 +2,11 @@
 
 # This script changes the file extension of all JPEG images that incorrectly
 # match the "IMG_*.HEIC" file name pattern from ".HEIC" to ".JPG".
+# It scans the current directory and all subdirectories.
 
-for file in $(file IMG_*.HEIC | grep JPEG | cut -d ':' -f1); do
-  mv "$file" "${file%.*}.JPG" ;
-done;
+find . -iname "IMG_*.HEIC" -print0 | while IFS= read -r -d $'' file; do
+  if file "$file" | grep -q "JPEG"; then
+    echo "Renaming \"$file\"..."
+    mv "$file" "${file%.*}.JPG" ;
+  fi
+done
