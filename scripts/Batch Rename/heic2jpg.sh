255ed3e9800a418f55cab7979b00d30a12b39c9b
Xie Yanbo <xieyanbo@gmail.com>
2025-08-23 12:07:50 +0800

    feat: recognize file by mime type

--- scripts/Batch Rename/heic2jpg.sh
+++ scripts/Batch Rename/heic2jpg.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # This script changes the file extension of all JPEG images that incorrectly
-# match the "IMG_*.HEIC" file name pattern from ".HEIC" to ".JPG".
+# match the "*.HEIC" file name pattern from ".HEIC" to ".JPG".
 # It scans the current directory and all subdirectories.
 
-find . -iname "IMG_*.HEIC" -print0 | while IFS= read -r -d $'' file; do
-  if file "$file" | grep -q "JPEG"; then
+find . -iname "*.HEIC" -print0 | while IFS= read -r -d $'' file; do
+  if file --brief --mime-type -- "$file" | grep -q '^image/jpeg$'; then
     echo "Renaming \"$file\"..."
-    mv "$file" "${file%.*}.JPG" ;
+    mv -- "$file" "${file%.*}.JPG" ;
   fi
 done
